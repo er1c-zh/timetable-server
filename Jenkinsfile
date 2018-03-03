87259f44c21550c61840cb59de3593fb5961cafb
eric <ericzhao96@hotmail.com>
2018-03-03 09:56:22 +0800

    [add] 1 Jenkins stage deploy [revise] docker version

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,4 +8,10 @@ node {
         echo '[STAGE][mvn package] mvn package'
         sh 'mvn package'
     }
+
+    stage('deploy to docker') {
+        sh 'docker rm -f uestc-timetable-server'
+        sh 'sleep 1'
+        sh 'dcoker run -d -p 80:1220 --name uestc-timetable-server ek1o/uestc-timetable-server:latest'
+    }
 }
--- src/main/java/cn/ek1o/timetable/server/TimetableServer.java
+++ src/main/java/cn/ek1o/timetable/server/TimetableServer.java
@@ -1,11 +1,8 @@
 package cn.ek1o.timetable.server;
 
-import io.netty.handler.codec.HeadersUtils;
 import io.netty.handler.codec.http.HttpHeaderNames;
-import io.netty.handler.codec.http.HttpHeaderValues;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServerRequest;
 
 public class TimetableServer {
